a9970380a5c689d90da490765fb39f19cac216dd
BT_4N2_11 <samzad7698@bths.edu>
2022-09-23 12:30:44 -0400

    First change to calculate area of first rectangle

---
+++ src/U2L2Runner.java
@@ -0,0 +1,10 @@
+public class U2L2Runner {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle (150, 200);
+        rect1.printRectangleInfo();
+
+    }
+
+
+    }
+
